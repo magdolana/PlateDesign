ff1f2f5e766cfd1e4b03f413229fe65e85d29bb9
mailrameshs <mailrameshs25@gmail.com>
2015-02-24 01:09:43 -0500

    Added lab data models and injected them using requirejs

--- ui/js/HelloWorld/HelloWorldCtrl.js
+++ ui/js/HelloWorld/HelloWorldCtrl.js
@@ -2,7 +2,8 @@ define(['angular'], function (angular) {
     'use strict';
     return angular.module('app.helloworld.ctrls', []).controller('helloCtrl', [
         '$scope', function($scope) {
-            $scope.hello = 'Hello World!!';
+            var user = LabUser({userName:'qaadmin1'});
+            $scope.hello = 'Hello ' + user.userName;
         }
     ]).controller('helloCtrlService', function($scope, helloWorldService) {
         helloWorldService.hello("http://0.0.0.0:9090", "James").then(
