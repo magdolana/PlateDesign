ece0f65dcfc04738b144518ad9c68e56abbdd611
rameshsubramania <ramesh.s@axleinfo.com>
2015-02-25 11:55:59 -0500

    Auto generate lsmodels file with grunt build

--- ui/js/HelloWorld/HelloWorldCtrl.js
+++ ui/js/HelloWorld/HelloWorldCtrl.js
@@ -1,4 +1,4 @@
-define(['angular'], function (angular) {
+define(['angular', 'lsModels'], function (angular) {
     'use strict';
     return angular.module('app.helloworld.ctrls', []).controller('helloCtrl', [
         '$scope', function($scope) {
