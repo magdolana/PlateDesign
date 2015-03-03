1a45a4be82baf038f31689aaf0dd4f61b38393db
James Williams <jwsorad@gmail.com>
2015-03-03 13:14:02 -0500

    reverted name to ls-seed

--- ui/js/shared/main.js
+++ ui/js/shared/main.js
@@ -8,7 +8,7 @@ define(['angular', 'underscore'], function (angular) {
         return _.contains(['/404', '/pages/500', '/pages/login', '/pages/signin', '/pages/signin1', '/pages/signin2', '/pages/signup', '/pages/signup1', '/pages/signup2', '/pages/lock-screen'], path);
       };
       return $scope.main = {
-        brand: 'project HEDWIG',
+        brand: 'LS-Seed',
         name: 'Jane Doe'
       };
     }
